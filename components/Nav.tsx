8609000b1c8d51c3c1fc5ecad4874e4e2865bd35
abhay <abhayforwork02@gmail.com>
2024-07-28 01:08:33 +0530

    code

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,20 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa"
+
 import  Typography  from '@mui/material/Typography';
 import Box  from '@mui/material/Box';
 import React from 'react'
-import { CiMail } from "react-icons/ci";
-import { FaXTwitter } from "react-icons/fa6";
-import { useTheme } from "@emotion/react";
+import { useTheme } from "@mui/material";
 import { useMediaQuery } from "@mui/material";
+import NavLink from "./NavLink";
 
 const Nav = () => {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const navLinks = [{ src : "", link : "https://github.com/abhay2jn" },
+      {src : "CiMail", link : "https://github.com/abhay2jn" },
+      {src : "FaLinkedin", link : "https://github.com/abhay2jn" },
+      {src: "FaXTwitter", link : "https://github.com/abhay2jn" },
+    ];
 
 
   return (
@@ -19,11 +24,14 @@ const Nav = () => {
         maxWidth: matches ? "10.875rem" : "none"
     }} >
         <Typography sx = {{margin : matches ? "1.25rem" : 0}} variant="h1">Abhay Jain</Typography>
-        <Box>
-            <FaGithub width={20} height={20} />
-            <CiMail width={20} height={20} />
-            <FaLinkedin width={20} height={20} />
-            <FaXTwitter width={20} height={20} />
+        <Box sx={{ display  :"flex", justifyContent : "space-between", width : matches ? "10.875rem" : "12.5rem" }}>
+            {navLinks.map((navLink) => {
+              return(
+                <NavLink
+                src={navLink.src} link = {navLink.link}
+                key={navLink.src} />
+              );
+            })}
         </Box>
     </Box>
     </Box>
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,11 +1,13 @@
-import { FaGithub } from 'react-icons/fa'
 import React from 'react'
 import Link from 'next/link';
 
-const NavLink = ({src, link}: { src : string; link :string;}) => {
+
+
+
+const NavLink = ({src, link}: { src: string; link :string;}) => {
   return (
-    <Link href={}>
-        <FaGithub width={20} height={20} />
+    <Link href={link}>
+        < src={src} width={20} height={20} />
     </Link>
   )
 }
