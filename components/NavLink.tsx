af0f8225d954ebaadd1eff2c8e8f0cd2567bcc67
abhay <abhayforwork02@gmail.com>
2024-07-31 23:29:01 +0530

    nav

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -7,9 +7,9 @@ import Image from 'next/image';
 
 const NavLink = ({src, link}: { src: string; link :string;}) => {
   return (
-    <Link href={link}>
+    <a href={link}>
         <Image src={src} alt='github' width={20} height={20} />
-    </Link>
+    </a>
   )
 }
 
