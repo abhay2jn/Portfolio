b543027c74e1b589aea867e8d087ab5c758c454c
abhay <abhayforwork02@gmail.com>
2024-08-01 01:24:09 +0530

    Skills

---
+++ data/index.ts
@@ -0,0 +1,11 @@
+export const skills = [
+    {skill : "HTML", level : "Intermediate"},
+    {skill : "CSS", level : "Intermediate"},
+    {skill : "Tailwind", level : "Intermediate"},
+    {skill : "JavaScript", level : "Intermediate"},
+    {skill : "ReactJS", level : "Intermediate"},
+    {skill : "NextJS", level : "Intermediate"},
+    {skill : "NodeJS", level : "Intermediate"},
+    {skill : "MongoDB", level : "Intermediate"},
+    {skill : "TypeScript", level : "Intermediate"},
+];
