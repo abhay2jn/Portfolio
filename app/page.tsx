da29faf1b181e4a4d5822d7c8c56991cdcd1682b
abhay <abhayforwork02@gmail.com>
2024-08-01 01:22:34 +0530

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Nav from "@/components/Nav";
 import Photo from "@/components/Photo";
+import Skills from "@/components/Skills";
 import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
@@ -11,6 +12,7 @@ const Home = () => {
       <Container maxWidth = "sm">
         <Nav  />
         <Photo />
+        <Skills />
       </Container>
     </Box>
   );
