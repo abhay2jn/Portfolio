edb4413ca7f5053ca764ee5a518bfe86f0e00dd2
abhay <abhayforwork02@gmail.com>
2024-08-02 02:03:05 +0530

    data

--- data/index.ts
+++ data/index.ts
@@ -9,3 +9,33 @@ export const skills = [
     {skill : "MongoDB", level : "Intermediate"},
     {skill : "TypeScript", level : "Intermediate"},
 ];
+export const projects = [
+    {
+        viewProject: "https://e-commerce-website-eight-rho.vercel.app/",
+        code: "https://github.com/abhay2jn/E-Commerce-Website",
+        image: "/e-commerce-website.png",
+        title: "E-COMMERCE-WEBSITE",
+        skills: ["ReactJS"],
+    },
+    {
+        viewProject: "https://comment-project-three.vercel.app/",
+        code: "https://github.com/abhay2jn/Comment-Project",
+        image: "/comment-website.png",
+        title: "COMMENT-WEBSITE",
+        skills: ["REACTJS"],
+    },
+    {
+        viewProject: "https://fake-users-management-project.vercel.app/",
+        code: "https://github.com/abhay2jn/fake-users-management-project",
+        image: "/users.png",
+        title: "FAKE-USERS-MANAGEMENT",
+        skills: ["NextJS"],
+    },
+    {
+        viewProject: "https://to-do-list-eight-mu-87.vercel.app/",
+        code: "https://github.com/abhay2jn/To-do-list-",
+        image: "/to-do.png",
+        title: "TO-DO LIST",
+        skills: ["ReactJS"],
+    },
+];
