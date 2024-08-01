61a9422b3ec5c3cc91ac5f89a2c3cba097eb820d
abhay <abhayforwork02@gmail.com>
2024-08-02 02:00:27 +0530

    Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import MUIButton from "@mui/material/Button"
 import { COLOR } from '@/styles/colors';
+import { SxProps } from '@mui/material';
 
-function Button({ children , link} : { children : React.ReactElement | string,
-    link  : string,
+function Button({ children, link, sx} : { children : React.ReactElement | string,
+    link  : string, sx?: SxProps;
 }) {
   return (
     <a href={link}>
-    <MUIButton sx={{color : COLOR.white, fontWeight : 700}}>
+    <MUIButton sx={{color : COLOR.white, fontWeight : 700, fontSize: "1rem", padding: 0
+      , ...sx
+    }}>
       <u 
     style={{textDecoration: "underline", textDecorationColor : 'blueviolet',
-     textUnderlineOffset : "0.7rem",
+     textUnderlineOffset : "1rem",
      textDecorationThickness: "0.25rem"}} >
       {children}</u></MUIButton>
     </a>
