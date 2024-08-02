8b3b54da2e88fdfd98e4676723a4024e4fadc20e
abhay <abhayforwork02@gmail.com>
2024-08-03 00:49:21 +0530

    Image

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -12,19 +12,30 @@ function Photo() {
   return (
     <Box sx={{ display : "flex",
      flexDirection : matches ? "column" : 'row-reverse'
-     , alignItems : 'center',
-     transform: "translateY(-22px)"}}>
+     , alignItems : 'center', justifyContent : matches ? "none" : "space-around",
+     transform: matches ? "translateY(-22px)" : "none"}}>
+      {matches ? (
         <Image src="/pankaj_swag.png" 
         alt='swag' 
         width={174}
         height={383}
         style={{ marginBottom : "2.5rem"}}
          />
-         <Box sx={{ textAlign : 'center'}}>
-            <Typography variant='h2'>Nice to meet you! I’m <u style={{textDecoration: "underline", textDecorationColor : "blueviolet",
+      ) : (
+        <Image src="/pankaj_swag.png" 
+        alt='swag' 
+        width={445}
+        height={720}
+        style={{ marginBottom : "2.5rem"}}
+         />
+      )}
+         <Box sx={{ textAlign : matches ? "center": "left", maxWidth : matches ? "none" : "44.3125rem",
+          marginTop : matches ? "none" : "8.0625rem"
+          }}>
+            <Typography variant='h2' sx={{fontSize: matches ? "auto" : "5.5rem"}}>Nice to meet you! I’m <u style={{textDecoration: "underline", textDecorationColor : "blueviolet",
                 textUnderlineOffset: "0.4rem"
             }}>Abhay Jain</u>.</Typography>
-            <Typography variant='body1' sx={{margin : "1.5rem"}}>Based in India, I’m a full-stack developer passionate about building accessiable website that are nice.
+            <Typography variant={matches ?'body1' : "body2"} sx={{margin : "1.5rem 0" }}>Based in India, I’m a full-stack developer passionate about building accessiable website that are nice.
             </Typography>
             <Button link='mailto:abhayforwork02@gmail.com'>CONTACT ME</Button>
          </Box>
