2850ad195add071f39e16b7f3598e070ff0c0097
abhay <abhayforwork02@gmail.com>
2024-08-03 00:49:40 +0530

    Projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,13 +3,20 @@ import Box from '@mui/material/Box'
 import Image from 'next/image'
 import React from 'react'
 import Button from './Button';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const Project = ({viewProject,code,image,title,skills}: {
   viewProject: string; code: string; image: string; title: string; skills: string[];
 }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <Box sx={{margin: "2.5rem 0"}}>
-      <Image src={image} alt={title} width={343} height={253} />
+      {matches ? (
+      <Image src={image} alt={title} width={343} height={253} /> 
+      ) : (
+        <Image src={image} alt={title} width={545} height={400} />
+      )}
       <Typography variant='h4' sx={{margin: "1.25rem 0 0.5rem"}}>{title}</Typography>
       <Box sx={{ display: "flex"}}>
       {skills.map((skill) => {
