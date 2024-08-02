424298d4a1822d36b19a9de43777d10b65c6602d
abhay <abhayforwork02@gmail.com>
2024-08-03 00:50:25 +0530

    Projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,14 +4,22 @@ import React from 'react'
 import Button from './Button'
 import { projects } from '@/data'
 import Project from './Project'
+import { useMediaQuery, useTheme } from '@mui/material'
 
 const Projects = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <Box sx={{margin: "5rem 0"}}>
-        <Box sx={{ display : 'flex', justifyContent: "space-between"}}>
-        <Typography variant='h2'>Projects</Typography>
+        <Box sx={{ display : 'flex', justifyContent: "space-between", alignItems : "center",
+        }}>
+        <Typography variant='h2'sx={{fontSize: matches ? "auto" : "5.5rem"}}>Projects</Typography>
         <Button link='mailto:abhayforwork02@gmail.com'>CONTACT ME</Button>
         </Box>
+        <Box sx={{display: "flex" ,flexDirection : matches ? "column" : "row",
+          flexWrap : matches ? "nowrap" : "wrap",
+          justifyContent : matches ? "none" : "space-between"
+        }}>
         {projects.map((project) => {
           return <Project 
           viewProject={project.viewProject}
@@ -22,6 +30,7 @@ const Projects = () => {
           key={project.title}
           />
         })}
+        </Box>
     </Box>
   )
 }
