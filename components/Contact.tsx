d9fb798f6324e6312b11886d9ca976d2d988a38a
abhay <abhayforwork02@gmail.com>
2024-08-03 00:49:03 +0530

    Contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,26 +5,35 @@ import { COLOR } from '@/styles/colors'
 import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import Button from './Button'
-import { Container } from '@mui/material'
+import { Container, useMediaQuery, useTheme } from '@mui/material'
 import Nav from './Nav'
 
 const Contact = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return (
-    <Container maxWidth="sm" sx={{backgroundColor: COLOR.darkGray, paddingBottom: "3.75rem"}}>
-    <Box sx={{textAlign : "center",
-    padding: "3.5rem 0"}}>
-        <Typography variant='h2'>Contact</Typography>
-        <Typography variant='body1' sx={{marginTop: "0.4rem"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, nisi laborum rerum doloribus sapiente quas optio q</Typography>
-        <FormControl sx={{width: "90%"}}>
+    <Container maxWidth="xl" sx={{backgroundColor: COLOR.darkGray, paddingBottom: "3.75rem"}}>
+      <Container maxWidth="lg">
+    <Box sx={{textAlign : matches ? "center" : "left",
+    padding: "3.75rem 0",
+    display : "flex",
+    flexDirection: matches ? "column" : "row",
+    justifyContent: matches ? "none" : "space-between"}}>
+      <Box sx={{width : matches ? "auto" : "27.8125rem"}}>
+        <Typography variant='h2' sx={{fontSize : matches ? "auto" : "5.5rem"}}>Contact</Typography>
+        <Typography variant={matches ? 'body1' : "body2"} sx={{marginTop: "0.4rem"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, nisi laborum rerum doloribus sapiente quas optio q</Typography>
+      </Box>
+        <FormControl sx={{width: matches ? "auto"  : "27.8125rem"}}>
             <TextField variant='standard' label="Name"id='name' />
             <TextField variant='standard' label="Email"id='email' />
             <TextField variant='standard' label="Message"id='message'
             multiline rows={4} />
-            <Button link='#send message' sx={{margin: "2rem 0 1rem 0", paddingLeft: "11.75rem"}}>SEND MESSAGE</Button>
+            <Button link='#send message' sx={{margin: "2rem 0 1rem 0"}}>SEND MESSAGE</Button>
         </FormControl>
         </Box>
         <hr style={{width: "100%", margin: "1rem auto"}}></hr>
         <Nav />
+        </Container>
     </Container>
   )
 }
