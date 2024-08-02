cfab647a87159114134984cf080ee3f53443bae0
abhay <abhayforwork02@gmail.com>
2024-08-03 00:51:18 +0530

    Skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,12 +1,17 @@
 import  Typography  from '@mui/material/Typography'
 import  Box from '@mui/material/Box'
 import React from 'react'
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const Skill = ({skill, level}: { skill: string; level :string;}) => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down("sm"));
     return(
-    <Box sx={{ margin: "1.5rem 0"}}>
-        <Typography variant='h3'>{skill}</Typography>
-        <Typography variant='body1'>{level}</Typography>
+    <Box sx={{ margin: matches ? "1.5rem 0" : "3.625rem 0",
+        marginRight : matches ? "0" : "6.25rem", width : matches ? "auto" : "17.5rem"
+    }}>
+        <Typography variant='h3' sx={{fontSize : matches ? "auto" : "3rem"}}>{skill}</Typography>
+        <Typography variant={matches ? 'body1' : "body2"}>{level}</Typography>
     </Box>
     );
 }
