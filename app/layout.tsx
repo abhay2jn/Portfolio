155a88b80fad3f9f0b7d3e434b6fb531d17163bc
abhay <abhayforwork02@gmail.com>
2024-09-17 17:31:34 +0530

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="use-credentials"
-        />
+          crossOrigin="anonymous"
+        ></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Outfit:wght@100..900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
-        />
+        ></link>
       </head>
       <body className={inter.className} style={{}}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
