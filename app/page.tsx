8fd89f4049c174e199653b183048350f06478774
abhay <abhayforwork02@gmail.com>
2024-09-19 16:26:24 +0530

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
 "use client";
-import StarsCanvas from "@/components/Background";
 import Contact from "@/components/Contact";
 import Nav from "@/components/Nav";
 import Photo from "@/components/Photo";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import { Container } from "@mui/material";
+import { Container, useMediaQuery, useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return (
-    <Box>
-      <StarsCanvas />
+    <Box sx={{ backgroundColor: "#151515" }}>
+      <Image
+        src="/pattern-rings.svg"
+        alt="background"
+        width={530}
+        height={129}
+        style={{
+          position: "absolute",
+          transform: matches
+            ? "translate(-420px, 133px)"
+            : "translate(-100px, 133px)",
+        }}
+      />
       <Container maxWidth="lg">
         <Nav />
         <Photo />
